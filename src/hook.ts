b9267ea42bfd8bb375f2d31aa5e54dd73b98402d
wulu007 <dabaobeix@gmail.com>
2026-03-14 15:51:47 +0800

    feat: add getter and setter methods to enhance property hooking functionality

--- src/hook.ts
+++ src/hook.ts
@@ -50,5 +50,41 @@ export const hook = (() => {
     getNativeFunction<T extends AnyFunction>(hookedFn: T): T | undefined {
       return nativeFnMap.get(hookedFn) as T | undefined
     },
+    getter<T extends object, K extends keyof T>(
+      obj: T,
+      propName: K,
+      handler: HookHandler,
+    ) {
+      const desc = getDescriptor(obj, propName)
+      const originalGet = desc?.get as AnyFunction
+
+      if (!desc || !originalGet || typeof originalGet !== 'function')
+        return
+
+      // 使用你的神器 createWrapper 进行完美伪装
+      // (原生的 getter 通常名字叫 "get contentWindow")
+      const wrapper = createWrapper(handler, originalGet)
+
+      // 录入指纹库，闭环形成！
+      nativeStrMap.set(wrapper, $reflectApply($toString, originalGet, []))
+      nativeFnMap.set(wrapper, originalGet)
+      $defineProperty(obj, propName, { ...desc, get: wrapper })
+    },
+    setter<T extends object, K extends keyof T>(
+      obj: T,
+      propName: K,
+      handler: HookHandler,
+    ) {
+      const desc = getDescriptor(obj, propName as string | symbol)
+      const originalSet = desc?.set as AnyFunction
+
+      if (!desc || !originalSet || typeof originalSet !== 'function')
+        return
+
+      const wrapper = createWrapper(handler, originalSet)
+      nativeStrMap.set(wrapper, $reflectApply($toString, originalSet, []))
+      nativeFnMap.set(wrapper, originalSet)
+      $defineProperty(obj, propName, { ...desc, set: wrapper })
+    },
   }
 })()
